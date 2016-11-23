7eee2c705471698a04b3bb41063360001210d1f6
Alan Fidelino <afidedev26@gmail.com>
2016-11-23 12:59:24 +1100

    Use lodash and isString() and uniq() functions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #This is the repository for my studies regarding The Complete Node.js Developer Course 2.0 by Andrew Mead
 
 ###Section and Projects
-1. Note App
+1. Note App - see versions for lessons
--- note-app/app.js
+++ note-app/app.js
@@ -6,13 +6,17 @@ console.log("Starting app.js");
 const fs    = require('fs');
 const os    = require('os');
 const notes = require('./notes.js')
+const _     = require('lodash');
 
-// Exercise 1 for files branch
-// var result  = notes.addNote();
-// console.log(result);
+// Exercise 1 for 3rd Party modules - _.isString()
+// console.log(_.isString(false));
+// console.log(_.isString('Alan'));
 
-var sum = notes.add(5,9);
-console.log('Result:',sum);
+// Exercise 2 for 3rd Party modules - _.uniq()
+var filteredArray = _.uniq(["Alan", "Alan", 26, 11,11, 44,45,75]);
+
+// The function will return a new array without the duplicates
+console.log(filteredArray, 'New array length:', filteredArray.length);
 // var user = os.userInfo();
 
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}. You are now ${notes.age} soon to be ${notes.age + 1}.`);
