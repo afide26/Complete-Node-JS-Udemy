88ee131b15a50f8b69153e3468c6c6c20a235785
Alan Fidelino <afidedev26@gmail.com>
2016-11-23 13:29:45 +1100

    Get user inputusing process.argv

--- note-app/app.js
+++ note-app/app.js
@@ -3,4 +3,19 @@ console.log("Starting app.js");
 const fs    = require('fs');
 const _     = require('lodash');
 
-const notes = require('./notes.js')
+const notes = require('./notes.js');
+console.log(process.argv)
+var command = process.argv[2];
+console.log('Command:',command.toUpperCase());
+
+if(command.toLowerCase() === 'add'){
+  console.log("Adding new note");
+}else if(command.toLowerCase() ==='list'){
+  console.log("Listing all notes");
+}else if(command.toLowerCase() ==="read"){
+  console.log("Reading a note");
+}else if(command.toLowerCase() ==="remove"){
+  console.log("Removing a note");
+}else{
+  console.log("Command not recognised");
+}
