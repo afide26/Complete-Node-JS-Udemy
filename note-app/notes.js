ea545ca51edbebcbb53415c60df91d518576b196
Alan Fidelino <afidedev26@gmail.com>
2016-11-23 21:46:46 +1100

    3.16 Adding and Saving notes

--- note-app/notes.js
+++ note-app/notes.js
@@ -1,8 +1,36 @@
 console.log("Starting notes.js");
-
+const fs = require('fs');
 
 var addNote = (title, body)=>{
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+              title:title,
+              body: body
+            };
+
+  // Write a try catch to solve the problem if notes-data.json does not exist
+  // Write code to ensure the notes-data.json will not be wiped out
+  try{
+    var noteString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(noteString);
+  }catch(e){
+
+  }
+
+ // Use ES6 style without brackets to return boolean
+ // to return an array using filter
+ var duplicateNotes = notes.filter((note)=> note.title === title);
+
+
+ // Check if there are duplicate notes
+ if(duplicateNotes.length === 0){
+  notes.push(note);
+
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+ }else{
+  console.log("The title already exists");
+ }
+
 };
 
 var getAll = ()=>{
