f6185a991046b72a71f12c56bfac98c5a26fef16
Alan Fidelino <afidedev26@gmail.com>
2016-11-23 13:12:04 +1100

    Clean up files

--- note-app/app.js
+++ note-app/app.js
@@ -1,23 +1,6 @@
 console.log("Starting app.js");
-// Notes
-// 1.0 Require fs module to use it and store in const fs
-// 1.1 Read more here - https://nodejs.org/api/fs.html#fs_fs_appendfile_file_data_options_callback
-// 2.0 Read more os.userInfo([options]) - https://nodejs.org/api/os.html#os_os_userinfo_options
+
 const fs    = require('fs');
-const os    = require('os');
-const notes = require('./notes.js')
 const _     = require('lodash');
 
-// Exercise 1 for 3rd Party modules - _.isString()
-// console.log(_.isString(false));
-// console.log(_.isString('Alan'));
-
-// Exercise 2 for 3rd Party modules - _.uniq()
-var filteredArray = _.uniq(["Alan", "Carl", "Tin", "Tin", 26, 11,11, 44,45,75]);
-
-// The function will return a new array without the duplicates
-console.log(filteredArray, 'New array length:', filteredArray.length);
-// var user = os.userInfo();
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}. You are now ${notes.age} soon to be ${notes.age + 1}.`);
-
+const notes = require('./notes.js')
