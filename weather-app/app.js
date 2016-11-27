67341ddf8f22274e68ddf4c04312414c29687bb4
Alan Fidelino <afidedev26@gmail.com>
2016-11-27 20:57:24 +1100

    4.8 Callback Errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,8 +21,15 @@ request({
   json: true
 
 },(error, response, body)=>{
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    console.log('Unable to connect to the Google Servers')
+  }else if(body.status === "ZERO_RESULTS"){
+    console.log("Unable to find that address");
+  }else if(body.status === "OK"){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
+
 
 });
