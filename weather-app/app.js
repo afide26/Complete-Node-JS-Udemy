9b3ff1b7b28f46e89e0401d1321da82f8f7f1a04
Alan Fidelino <afidedev26@gmail.com>
2016-11-27 20:28:29 +1100

    4.7 Encoding user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
 
-// Use the loaded request function
-// The first argument is an options object{ }
-// The second argument is a callback function
+const argv = yargs
+             .options({
+              a:{
+                demand: true,
+                alias: 'address',
+                describe: 'Address to fetch weather for',
+                string: true
+              }
+             })
+             .help()
+             .alias('help', 'h')
+             .argv;
+console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url:'https://maps.googleapis.com/maps/api/geocode/json?address=10%20lombard%20street%20philadelphia',
+  url:"https://maps.googleapis.com/maps/api/geocode/json?address="+ encodedAddress,
   json: true
 
 },(error, response, body)=>{
-  // As the request file(JSON) was converted to an object
-  // The response needs to be converted to JSON, this time with
-  // 3 arguments, the object, undefined and the 3rd argument which is the number of lines each object should have
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
