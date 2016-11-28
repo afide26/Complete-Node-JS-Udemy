31ade58d4f7f91a8e2a976462aff388dcf1297cc
Alan Fidelino <afidedev26@gmail.com>
2016-11-28 19:26:30 +1100

    4.10 Wiring Up Weather Search

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,45 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-             .options({
-              a:{
-                demand: true,
-                alias: 'address',
-                describe: 'Address to fetch weather for',
-                string: true
-              }
-             })
-             .help()
-             .alias('help', 'h')
-             .argv;
+// const argv = yargs
+//              .options({
+//               a:{
+//                 demand: true,
+//                 alias: 'address',
+//                 describe: 'Address to fetch weather for',
+//                 string: true
+//               }
+//              })
+//              .help()
+//              .alias('help', 'h')
+//              .argv;
 
 
-geocode.geocodeAddress(argv.address, (errorMessage,results)=>{
+// geocode.geocodeAddress(argv.address, (errorMessage,results)=>{
 
-  if(errorMessage){
-    console.log(errorMessage);
+//   if(errorMessage){
+//     console.log(errorMessage);
+//   }else{
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+
+// });
+
+// https://api.darksky.net/forecast/d79243fcc7770a73c43306d466b29990/long,lat
+const request = require('request');
+request({
+  url:"https://api.darksky.net/forecast/d79243fcc7770a73c43306d466b29990/-33.8533939,150.9132068",
+  json: true
+},(error, response, body)=>{
+  if(!error && response.statusCode === 200){
+    console.log(parseFloat((body.currently.temperature-32)*5/9).toFixed(1));
   }else{
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log("Unable to fetch weather");
   }
-
 });
+
+
+
+
+
+
