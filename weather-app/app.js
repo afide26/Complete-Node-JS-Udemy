901ace7363bed6916d6ed0a5c932fe3b07447245
Alan Fidelino <afidedev26@gmail.com>
2016-11-28 20:31:05 +1100

    4.11 Chaining Callbacks Together

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,42 +1,54 @@
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode');
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-// const argv = yargs
-//              .options({
-//               a:{
-//                 demand: true,
-//                 alias: 'address',
-//                 describe: 'Address to fetch weather for',
-//                 string: true
-//               }
-//              })
-//              .help()
-//              .alias('help', 'h')
-//              .argv;
+const argv = yargs
+             .options({
+              a:{
+                demand: true,
+                alias: 'address',
+                describe: 'Address to fetch weather for',
+                string: true
+              }
+             })
+             .help()
+             .alias('help', 'h')
+             .argv;
 
 
-// geocode.geocodeAddress(argv.address, (errorMessage,results)=>{
+geocode.geocodeAddress(argv.address, (errorMessage,results)=>{
 
-//   if(errorMessage){
-//     console.log(errorMessage);
-//   }else{
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-
-// });
-
-// https://api.darksky.net/forecast/d79243fcc7770a73c43306d466b29990/long,lat
-const request = require('request');
-request({
-  url:"https://api.darksky.net/forecast/d79243fcc7770a73c43306d466b29990/-33.8533939,150.9132068",
-  json: true
-},(error, response, body)=>{
-  if(!error && response.statusCode === 200){
-    console.log(parseFloat((body.currently.temperature-32)*5/9).toFixed(1));
+  if(errorMessage){
+    console.log(errorMessage);
   }else{
-    console.log("Unable to fetch weather");
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults)=>{
+      if(errorMessage){
+        console.log(errorMessage);
+      }else{
+         console.log(`${results.latitude}, ${results.longitude}`);
+        console.log(`It is currently ${weatherResults.temperature} degrees Celsius here but it feels like ${+parseInt(weatherResults.apparentTemperature)} degrees Celsius.`);
+      }
+    });
   }
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ weather-app/weather/weather.js
@@ -0,0 +1,25 @@
+const request = require('request');
+var toCelsius = (temp)=>{
+  return parseFloat((temp-32)*5/9).toFixed(1);
+}
+var getWeather = (lat,lng, callback)=>{
+  request({
+    url:"https://api.darksky.net/forecast/d79243fcc7770a73c43306d466b29990/"+lat+","+lng,
+    json: true
+  },(error, response, body)=>{
+    if(error){
+      callback('Unable to fetch weather from forecast.io servers');
+    }else if(response.statusCode ===400){
+      callback("Unable to fetch weather, check your input data");
+    }else if(response.statusCode === 200){
+      callback(undefined, {
+        temperature: toCelsius(body.currently.temperature),
+        apparentTemperature: toCelsius(body.currently.apparentTemperature)
+      });
+    }
+  });
+};
+
+
+
+module.exports.getWeather = getWeather;
