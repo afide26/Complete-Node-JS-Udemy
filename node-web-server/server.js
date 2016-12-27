b64d4f1fbe486939c2cc4352132489771755d12f
Alan Fidelino <afidedev26@gmail.com>
2016-12-27 15:46:21 +1100

    5.4 Advanced Templating

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -8,6 +8,18 @@ var port = process.env.PORT || 3000;
 
 // Set-up the middleware
 app.use(express.static(__dirname+'/public'));
+
+// Set-up and register partials in hbs
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Set-up and register a helper in hbs
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+})
 // Set-up the templating engine
 app.set("view engine", "hbs");
 
@@ -15,16 +27,13 @@ app.set("view engine", "hbs");
 app.get("/", (req, res)=>{
   res.render("home.hbs", {
     pageTitle: "Home Page",
-    currentYear: new Date().getFullYear(),
-    welcomeMessage: "Welcome to the home page"
+    welcomeMessage: "Welcome to my website"
   })
 });
 
 app.get("/about", (req, res)=>{
   res.render('about.hbs', {
-    pageTitle: "Some website",
-    // Syntax to get a dynamic current year
-    currentYear: new Date().getFullYear()
+    pageTitle: "Some website"
   });
 });
 
